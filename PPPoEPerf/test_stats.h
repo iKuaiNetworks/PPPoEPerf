a92be521806493a4dc4b8a776cc3f2765c986a72
xiaopeifeng <xiaopeifenng@gmail.com>
2016-09-30 15:30:05 +0800

    add license header

--- PPPoEPerf/test_stats.h
+++ PPPoEPerf/test_stats.h
@@ -1,3 +1,25 @@
+// ikuai is pleased to support the open source community by making PPPoEPerf available.
+// Copyright (C) 2016 ikuai. All rights reserved.
+
+// This file is part of PPPoEPerf.
+
+// PPPoEPerf is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// PPPoEPerf is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with PPPoEPerf.  If not, see <http://www.gnu.org/licenses/>.
+
+// ---
+// Author: Xiaopei Feng <xpfeng@ikuai8.com>
+
+
 #ifndef TEST_STATS_H_
 #define TEST_STATS_H_
 #include <ostream>
